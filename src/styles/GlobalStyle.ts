7c496d9fb19c51d1c994e1c8fc20eb7fa6394861
Camino Losada <camino.losada.alonso@gmail.com>
2023-05-12 23:40:46 +0200

    Create GlobalStyle

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*,::before,::after{
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    font: inherit;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul{
+    list-style: none;
+    padding-left: 0;
+    margin-bottom: 0;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    background-color: transparent;
+  }
+
+
+`;
+
+export default GlobalStyle;

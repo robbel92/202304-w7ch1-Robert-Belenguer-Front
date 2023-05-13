6e86f0b46be80450bb4adee73c17580c0a88e350
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-13 20:13:08 +0200

    Create Header

---
+++ src/components/App/Header/HeaderStyled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #95969c;
+  color: white;
+
+  .title {
+    font-size: 4rem;
+  }
+`;
+export default HeaderStyled;

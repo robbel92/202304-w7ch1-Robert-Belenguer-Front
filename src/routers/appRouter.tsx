ca73eb6aecca32e1934de4c46151c7d6d6f3c01f
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-16 22:07:46 +0200

    Add styles to LoginForm

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,7 +1,22 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
 
-const routes: RouteObject[] = [{ path: "/", element: <App /> }];
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: "/login",
+        element: <App />,
+      },
+    ],
+  },
+];
 
 const appRouter = createBrowserRouter(routes);
 

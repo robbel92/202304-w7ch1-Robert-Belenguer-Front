5626aeabc72e2f32e2a40e03919bec6145c5efe1
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-17 13:41:47 +0200

    Create RobotsPage

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,7 +1,7 @@
 import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
-import LoginFormPage from "../pages/LoginFormPage/LoginFormPage";
-import RobotsPage from "../pages/RobotsPage/RobotsPage";
+import LoginFormPage from "../pages/LoginFormPage/LoginFormPage.js";
+import RobotsPage from "../pages/RobotsPage/RobotsPage.js";
 
 const routes: RouteObject[] = [
   {
